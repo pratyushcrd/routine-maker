0d4153422295b0bb93289e280352914b583e9a68
pratyushcrd <pratyush.creed@gmail.com>
2018-06-23 17:40:20 +0530

    Initial commit

---
+++ app/containers/App.js
@@ -0,0 +1,25 @@
+import React, { Component, } from 'react'
+import PropTypes from 'prop-types'
+import DevTools from './DevTools'
+
+export default class App extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        {
+          (() => {
+            if (process.env.NODE_ENV !== 'production') {
+
+              return <DevTools />
+            }
+          })()
+        }
+      </div>
+    )
+  }
+}
---
+++ app/reducers/counter.js
@@ -0,0 +1,13 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER, } from '../actions/counter'
+
+export default function counter(state = 0, action) {
+  const newState = state
+  switch (action.type) {
+  case INCREMENT_COUNTER:
+    return newState + 1
+  case DECREMENT_COUNTER:
+    return newState - 1
+  default:
+    return state
+  }
+}
