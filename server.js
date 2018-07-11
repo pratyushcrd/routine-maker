18ee0ea53d7dc019257029029ceef6f3d2a06efd
pratyushcrd <pratyush.creed@gmail.com>
2018-07-12 00:40:28 +0530

    Server.js linting

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT ? process.env.PORT : 8181;
-const dist = path.join(__dirname, 'dist');
+const port = process.env.PORT ? process.env.PORT : 8181
+const dist = path.join(__dirname, 'dist')
 
-app.use(express.static(dist));
+app.use(express.static(dist))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(dist, 'index.html'));
-});
+  res.sendFile(path.join(dist, 'index.html'))
+})
 
 app.listen(port, (error) => {
   if (error) {
-    console.log(error); // eslint-disable-line no-console
+    console.log(error) // eslint-disable-line no-console
   }
-  console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
-});
+  console.info('Express is listening on port %s.', port) // eslint-disable-line no-console
+})
