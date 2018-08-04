536652ec1870641a1771596ce68251aed66dea87
pratyushcrd <pratyush.creed@gmail.com>
2018-08-04 20:32:31 +0530

    Grid structure added

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,7 +4,21 @@ import Grid from '@material-ui/core/Grid'
 
 function Home() {
   return (
-    <Grid container spacing={24} />
+    <Grid container spacing={24} >
+      <Grid item xs={9}>
+        <Grid container>
+          <Grid item xs={12}>
+            Chips
+          </Grid>
+          <Grid item xs={12}>
+            Details
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={3} >
+        Teachers Panel
+      </Grid>
+    </Grid>
   )
 }
 
