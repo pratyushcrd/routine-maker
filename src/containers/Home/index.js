976b6229b4181bba51e7ef7e7e15a46234d9be02
pratyushcrd <pratyush.creed@gmail.com>
2018-08-04 20:46:30 +0530

    Minor changes

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { connect, } from 'react-redux'
 import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
-function Home() {
+const styles = theme => ({
+  details: {
+    marginTop: `${theme.spacing.unit * 4}px`,
+  },
+})
+
+function Home(props) {
   return (
     <Grid container spacing={24} >
       <Grid item xs={9}>
         <Grid container>
-          <Grid item xs={12}>
+          <Grid item xs={12} >
             Chips
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} className={props.classes.details} >
             Details
           </Grid>
         </Grid>
@@ -22,11 +30,15 @@ function Home() {
   )
 }
 
-Home.propTypes = {}
+Home.propTypes = {
+  classes: PropTypes.shape({
+    details: PropTypes.string.isRequired
+  }).isRequired
+}
 
 
 function mapStateToProperties(state) {
   return Object.assign({}, state.input)
 }
 
-export default connect(mapStateToProperties)(Home)
+export default connect(mapStateToProperties)(withStyles(styles)(Home))
