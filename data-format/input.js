a474f037af13154ec6e5a553a68d61fd40124ea7
pratyushcrd <pratyush.creed@gmail.com>
2018-08-07 22:33:43 +0530

    data format update

--- data-format/input.js
+++ data-format/input.js
@@ -1,156 +1,65 @@
 module.exports = {
   days: [
     {
-      day: 'monday',
-      periods: 8,
-    }, {
-      day: 'tuesday',
-      periods: 8,
-    }, {
-      day: 'wednesday',
-      periods: 8,
-    }, {
-      day: 'thursday',
-      periods: 8,
-    }, {
-      day: 'friday',
-      periods: 8,
+      day: 'Monday',
+      periods: 7
+    },
+    {
+      day: 'Tuesday',
+      periods: 7
+    },
+    {
+      day: 'Wednesday',
+      periods: 8
+    },
+    {
+      day: 'Thursday',
+      periods: 6
+    }
+  ],
+  teachers: [
+    {
+      name: 'Manisha Gupta',
+      id: 'mg12'
+    },
+    {
+      name: 'Prakash Sinha',
+      id: 'ps12'
+    },
+    {
+      name: 'Mahesh Das',
+      id: 'md12'
+    }
+  ],
+  classList: [
+    {
+      className: '2',
+      subjects: [
+        {
+          name: 'English'
+        },
+        {
+          name: 'Science'
+        }
+      ],
+      sections: [
+        {
+          name: 'A'
+        },
+        {
+          name: 'B'
+        }
+      ]
     },
   ],
+  sections: [],
+  subjects: [],
   commonAreas: [
     {
-      name: 'Library',
+      name: 'Junior Section Library'
     },
     {
-      name: "Junior's Ground",
-    },
-    {
-      name: "Senior's Ground",
-    },
-  ],
-  teacher: [
-    {
-      id: 'ID234',
-      name: 'Kiran Rao',
+      name: 'Library'
     }
-  ],
-  classes1: [
-    {
-      className: '1',
-      sections: [{
-        name: 'A',
-        teacher: 'mh3124'
-      }, {
-        name: 'B',
-        teacher: 'mh3125'
-      }, {
-        name: 'C',
-        teacher: 'mh3126'
-      }]
-    },
-  ],
-  classes: [{
-    className: '1',
-    section: 'A',
-    classTeacher: '234',
-  }, {
-    className: '1',
-    section: 'B',
-    classTeacher: '',
-  }, {
-    className: '1',
-    section: 'C',
-    classTeacher: '',
-  }],
-  subjects: [
-    {
-      className: '1',
-      section: 'A',
-      subject: 'English',
-      periodsPerWeek: 6,
-      teachers: ['ID234']
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Hindi',
-      periodsPerWeek: 6,
-      teachers: ['ID276'],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Maths',
-      periodsPerWeek: 6,
-      teachers: [],
-      classLength: 2,
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Science',
-      periodsPerWeek: 6,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'History',
-      periodsPerWeek: 3,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Civics',
-      periodsPerWeek: 3,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Geography',
-      periodsPerWeek: 3,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'PT',
-      periodsPerWeek: 2,
-      teachers: [],
-      dependence: "Senior's Ground",
-      classLength: 2,
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Library',
-      periodsPerWeek: 1,
-      teachers: [],
-      dependence: 'Library'
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Environment',
-      periodsPerWeek: 1,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Arts',
-      periodsPerWeek: 1,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Music',
-      periodsPerWeek: 1,
-      teachers: [],
-    }, {
-      className: '1',
-      section: 'A',
-      subject: 'Sanskrit',
-      periodsPerWeek: 1,
-      teachers: [],
-    }
-  ],
-  globals: {
-    teacherMaxClassPerDay: {
-      8: 6
-    },
-    classTeacherFirstClass: true
-  },
+  ]
 }
