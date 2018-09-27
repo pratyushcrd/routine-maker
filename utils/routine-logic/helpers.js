3c03e73843dbb2acb8bcaa1241eea741588b8550
pratyushcrd <pratyush.creed@gmail.com>
2018-09-27 15:48:21 +0530

    Decoupled spreadDays

--- utils/routine-logic/helpers.js
+++ utils/routine-logic/helpers.js
@@ -19,9 +19,11 @@ exports.flattenDays = days => days
     }))
   ).reduce((a, b) => a.concat(b))
 
-exports.getDaysArray = days => days.map(({ day, periods }) => ({
+exports.spreadDaysOb = ({ day, periods }) => ({
   day,
   periods: Array(periods)
     .fill(1)
     .map((a, i) => a + i)
-}))
+})
+
+exports.getDaysArray = days => days.map(exports.spreadDaysOb)
