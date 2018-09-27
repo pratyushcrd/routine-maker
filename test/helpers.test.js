4dd97738cb887c8a798c0cf424d58413e055d5b5
pratyushcrd <pratyush.creed@gmail.com>
2018-09-27 13:54:52 +0530

    Added helper for flattenting days

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,25 +1,39 @@
 const { expect } = require('chai')
-const { generateDaysMap } = require('../utils/routine-logic/helpers')
+const { generateDaysMap, flattenDays } = require('../utils/routine-logic/helpers')
 
 const sample1 = [{
   day: 'Monday',
-  periods: 4,
+  periods: 2,
 }, {
   day: 'Tuesday',
-  periods: 3,
+  periods: 1,
 }]
 
 const sample1Result = {
   Monday: {
-    1: null, 2: null, 3: null, 4: null
+    1: null, 2: null
   },
   Tuesday: {
-    1: null, 2: null, 3: null
+    1: null
   }
 }
+
+const sample1ResultFlat = [{
+  day: 'Monday',
+  period: 1
+}, {
+  day: 'Monday',
+  period: 2
+}, {
+  day: 'Tuesday',
+  period: 1
+}]
 
 describe('Generate days map', () => {
   it('should make proper maps', () => {
     expect(generateDaysMap(sample1)).to.deep.equal(sample1Result)
+  })
+  it('should make proper maps', () => {
+    expect(flattenDays(sample1)).to.deep.equal(sample1ResultFlat)
   })
 })
--- utils/routine-logic/helpers.js
+++ utils/routine-logic/helpers.js
@@ -9,3 +9,12 @@ exports.generateDaysMap = days => days.reduce((acc, { day, periods }) => {
   return acc
 }, {})
 
+exports.flattenDays = days => days
+  .map(({ day, periods }) => Array(periods)
+    .fill(1)
+    .map((a, i) => a + i)
+    .map(period => ({
+      day,
+      period,
+    }))
+  ).reduce((a, b) => a.concat(b))
