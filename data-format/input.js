0b5e934dcc21677c2b9ecdcc0176f55534de67ad
pratyushcrd <pratyush.creed@gmail.com>
2018-12-26 08:16:37 +0530

    Changed input data

--- data-format/input.js
+++ data-format/input.js
@@ -31,6 +31,22 @@ module.exports = {
   ],
   teachers: [
     {
+      name: 'Mahima Rathore',
+      id: 'th16'
+    },
+    {
+      name: 'Hema Singh',
+      id: 'th15'
+    },
+    {
+      name: 'Sangeeta Basu',
+      id: 'th14'
+    },
+    {
+      name: 'Mukti Yadav',
+      id: 'th12'
+    },
+    {
       name: 'Surendar Singh',
       id: 'th7'
     },
@@ -61,6 +77,12 @@ module.exports = {
   ],
   classList: [
     {
+      className: '1'
+    },
+    {
+      className: '2'
+    },
+    {
       className: '6'
     }
   ],
@@ -75,6 +97,30 @@ module.exports = {
     },
     {
       className: '6',
+      section: 'C'
+    },
+    {
+      className: '1',
+      section: 'A'
+    },
+    {
+      className: '1',
+      section: 'B'
+    },
+    {
+      className: '1',
+      section: 'C'
+    },
+    {
+      className: '2',
+      section: 'A'
+    },
+    {
+      className: '2',
+      section: 'B'
+    },
+    {
+      className: '2',
       section: 'C'
     }
   ],
@@ -288,6 +334,486 @@ module.exports = {
       classLength: 1,
       commonArea: null,
       teacherId: 'th4'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'English',
+      periodsPerWeek: 8,
+      id: '@@1_A_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th7'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@1_A_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th6'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@1_A_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th5'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@1_A_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th4'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'SST',
+      periodsPerWeek: 6,
+      id: '@@1_A_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th3'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'Library',
+      periodsPerWeek: 1,
+      id: '@@1_A_library',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'V.ed',
+      periodsPerWeek: 2,
+      id: '@@1_A_v.ed',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th1'
+    },
+    {
+      className: '1',
+      section: 'A',
+      subject: 'Arts',
+      periodsPerWeek: 2,
+      id: '@@1_A_arts',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th4'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'English',
+      periodsPerWeek: 8,
+      id: '@@1_B_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th3'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@1_B_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th7'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@1_B_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th5'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@1_B_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th4'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'SST',
+      periodsPerWeek: 6,
+      id: '@@1_B_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th3'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'Librrary',
+      periodsPerWeek: 1,
+      id: '@@1_B_librrary',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'V.ed',
+      periodsPerWeek: 2,
+      id: '@@1_B_v.ed',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '1',
+      section: 'B',
+      subject: 'Arts',
+      periodsPerWeek: 2,
+      id: '@@1_B_arts',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th1'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'English',
+      periodsPerWeek: 8,
+      id: '@@1_C_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@1_C_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th1'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@1_C_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th4'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@1_C_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th7'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'SST',
+      periodsPerWeek: 6,
+      id: '@@1_C_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th3'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'Librrary',
+      periodsPerWeek: 1,
+      id: '@@1_C_librrary',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th5'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'V.ed',
+      periodsPerWeek: 2,
+      id: '@@1_C_v.ed',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '1',
+      section: 'C',
+      subject: 'Arts',
+      periodsPerWeek: 2,
+      id: '@@1_C_arts',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th2'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'English',
+      periodsPerWeek: 7,
+      id: '@@2_A_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th12'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@2_A_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th16'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@2_A_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th15'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@2_A_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th14'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'SST',
+      periodsPerWeek: 7,
+      id: '@@2_A_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th16'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'Art',
+      periodsPerWeek: 2,
+      id: '@@2_A_art',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th15'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'Music',
+      periodsPerWeek: 2,
+      id: '@@2_A_music',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th16'
+    },
+    {
+      className: '2',
+      section: 'A',
+      subject: 'PT',
+      periodsPerWeek: 1,
+      id: '@@2_A_pt',
+      classLength: 1,
+      commonArea: null,
+      teacherId: 'th14'
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'English',
+      periodsPerWeek: 7,
+      id: '@@2_B_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@2_B_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@2_B_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@2_B_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'SST',
+      periodsPerWeek: 7,
+      id: '@@2_B_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'Art',
+      periodsPerWeek: 2,
+      id: '@@2_B_art',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'Music',
+      periodsPerWeek: 2,
+      id: '@@2_B_music',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'B',
+      subject: 'PT',
+      periodsPerWeek: 1,
+      id: '@@2_B_pt',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'English',
+      periodsPerWeek: 7,
+      id: '@@2_C_english',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'Hindi',
+      periodsPerWeek: 7,
+      id: '@@2_C_hindi',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'Maths',
+      periodsPerWeek: 7,
+      id: '@@2_C_maths',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'Science',
+      periodsPerWeek: 7,
+      id: '@@2_C_science',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'SST',
+      periodsPerWeek: 7,
+      id: '@@2_C_sst',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'Art',
+      periodsPerWeek: 2,
+      id: '@@2_C_art',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'Music',
+      periodsPerWeek: 2,
+      id: '@@2_C_music',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
+    },
+    {
+      className: '2',
+      section: 'C',
+      subject: 'PT',
+      periodsPerWeek: 1,
+      id: '@@2_C_pt',
+      classLength: 1,
+      commonArea: null,
+      teacherId: null
     }
   ],
   commonAreas: [
